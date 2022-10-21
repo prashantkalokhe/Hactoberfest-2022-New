003047c149a17e67736b0aa3da10cf00363ea0d3
Tushal <tushalsharmagzb00@gmail.com>
2022-10-21 16:41:46 +0530

    revert

--- square_root_of_integer.cpp
+++ square_root_of_integer.cpp
@@ -1,17 +1,31 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int floorSqrt(int x)
 {
-	float ans = sqrt(x);
+	if (x == 0 || x == 1)
+		return x;
+	int st = 1, nd = x / 2, ans;
+	while (st <= nd)
+	{
+		int mid = (st + nd) / 2;
+		int sqr = mid * mid;
+		if (sqr == x)
+			return mid;
+		if (sqr <= x)
+		{
+			st = mid + 1;
+			ans = mid;
+		}
+		else
+			nd = mid - 1;
+	}
 	return ans;
 }
 
 int main()
 {
-	int x;
-	cout << "Enter the no. :";
-	cin >> x;
+	int x = 11;
 	cout << floorSqrt(x) << endl;
 	return 0;
 }
