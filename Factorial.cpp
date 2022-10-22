951a9c6dd2d572b5012209a070d4fb46a6311ba9
krrish singh <singhkrrish85@gmail.com>
2022-10-22 09:58:42 +0530

    adding factorial

--- Factorial.cpp
+++ Factorial.cpp
@@ -4,16 +4,20 @@ using namespace std;
 
 int main()
 {
-    int n,fact=1;
-    cout<<"Enter the number to find factorial ";
-    cin>>n;
-    
-    for(int i=1;i<=n;i++){
-        
-        fact=fact*i;
-    }
-    
-    cout<<"Factorial of the number is "<<fact;
+    int n, factorial = 1;
+    cout << "Enter the number to find factorial: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+        factorial = factorial * i;
+
+    cout << "Factorial of the number is: " << factorial;
 
     return 0;
 }
+/* 
+input: 
+Enter the number to find factorial: 5
+// factorial = 1*2*3*4*5 = 120
+output: Factorial of the number is: 120
+*/
