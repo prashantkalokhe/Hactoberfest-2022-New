627aa75b1661d0de4cfbdffe8b0135cd1d91e5bd
SheriffAbdullah <abdullah21110220@snuchennai.edu.in>
2022-10-22 20:35:31 +0530

    Addition of CurrencyDistanceTime Converter and EBBill Generator

---
+++ Java - Basic Programs/CurrencyDistanceTime Converter/Converters/CurrencyConverter.java
@@ -0,0 +1,69 @@
+package Converters;
+
+public class CurrencyConverter
+{
+    public double dollarToInr(int dollar) {
+        return (dollar * (double) 79.90);
+    }
+    public double dollarToInr(float dollar) {
+        return (dollar * (double) 79.90);
+    }
+    public double dollarToInr(double dollar) {
+        return (dollar * (double) 79.90);
+    }
+    
+    public double inrToDollar(int inr) {
+        return (inr / (double) 79.90);
+    }
+    public double inrToDollar(float inr) {
+        return (inr / (double) 79.90);
+    }
+    public double inrToDollar(double inr) {
+        return (inr / (double) 79.90);
+    }
+
+    public double euroToInr(int euro) {
+        return (euro * (double) 81.09);
+    }
+    public double euroToInr(float euro) {
+        return (euro * (double) 81.09);
+    }
+    public double euroToInr(double euro) {
+        return (euro * (double) 81.09);
+    }
+    
+    public double inrToEuro(int inr) {
+        return (inr / (double) 81.09);
+    }
+    public double inrToEuro(float inr) {
+        return (inr / (double) 81.09);
+    }
+    public double inrToEuro(double inr) {
+        return (inr / (double) 81.09);
+    }
+
+    public double yenToInr(int yen) {
+        return (yen / (double) 1.73);
+    }
+    public double yenToInr(float yen) {
+        return (yen / (double) 1.73);
+    }
+    public double yenToInr(double yen) {
+        return (yen / (double) 1.73);
+    }
+    
+    public double inrToYen(int inr) {
+        return (inr * (double) 1.73);
+    }
+    public double inrToYen(float inr) {
+        return (inr * (double) 1.73);
+    }
+    public double inrToYen(double inr) {
+        return (inr * (double) 1.73);
+    }
+}
+
+/*
+Note: Class & it's Methods MUST BE 'public' to be accessed 
+outside the package.
+*/
