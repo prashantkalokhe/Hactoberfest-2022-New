f50f038d88b580374ab4097d85eea801c8c402b3
dcryptau <68731756+dcryptau@users.noreply.github.com>
2022-10-23 21:03:21 +0530

    Create date to string converter.py

---
+++ Date Time in Python/date to string converter.py
@@ -0,0 +1,19 @@
+
+from datetime import datetime
+
+now = datetime.now() # current date and time
+
+year = now.strftime("%Y")
+print("year:", year)
+
+month = now.strftime("%m")
+print("month:", month)
+
+day = now.strftime("%d")
+print("day:", day)
+
+time = now.strftime("%H:%M:%S")
+print("time:", time)
+
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+print("date and time:",date_time)
